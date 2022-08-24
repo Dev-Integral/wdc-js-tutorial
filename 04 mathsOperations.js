59e9d99a694fdf7470fc93189985497d78ff62af
Taiwo Ayoola <tayoola13@yahoo.com>
2022-08-24 23:59:31 +0100

    Added new class files

---
+++ 04 mathsOperations.js
@@ -0,0 +1,29 @@
+
+    let sum = 4 + 5 // Expected result 9
+    let difference = 18 - 12 // Expected Result 6
+    let product = 3 * 4 // Expected result 12
+    let quotient = 20 / 5 //Expected result 4
+    let remainder = 22 % 5 // Expected result 2
+
+    // Incrementing/Decrementing a number
+    let myVariable = 25;
+ 
+    myVariable = myVariable + 1; // Expected result _;
+    myVariable++ // Short cut;
+    myVariable+=1 // Shortcut;
+
+    myVariable = myVariable - 1; // Expected result _;
+    myVariable-- // Short cut;
+    myVariable-=1 // Shortcut; Called augumented maths
+    
+    myVariable = myVariable * 2 // Expected Result
+    myVariable *=2  //shortcut
+    myVariable = myVariable - 1 //
+
+    // Decimals (Floating) numbers
+    let myDecimals = 20.50; // Perform operations the same way we did for integers
+
+    //BODMAS
+    let x = 5 * 4 - 10 / 10;
+    let y = 5 * (4 - 10 ) / 10;
+    console.log(x, y);
---
+++ 05 escapingQuotes.js
@@ -0,0 +1,35 @@
+// Escaping quotes
+let myVar = 'I\'ll come tomorrow';
+
+//Quoting quotes
+
+let anotherVar = "I'm coming home.";
+let yetAnother = ' He shouted! "LEAVE ME ALONE" ';
+let lastly = `we"ll show up for y'all`;
+
+let breakStrIntoLines = `Humpty dumpty sat on the wall \nHumpty dumpty sat on the wall \n`;
+let breakStrIntoLines2 = `Humpty dumpty sat on the wall \n\tHumpty dumpty sat on the wall`;
+
+//String Concatenation (+)
+var concatTest = 'This ' + 'Humpty dumpty sat on the wall'
+var myName = 'Taiwo';
+var greetings = "Hello, my name is " + myName + ", how are you?";
+var greetings2 = `Hello, my name is ${myName}, how are you?`;
+
+
+// Length of a string
+
+
+/*
+new line
+tab
+carriage return 
+backspace
+form feed
+*/
+
+console.log(breakStrIntoLines);
+console.log(breakStrIntoLines2);
+console.log(concatTest);
+console.log(greetings);
+console.log(greetings2);
